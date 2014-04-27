a62c949c959759c8ec91a5ddc003ec00b2335e6f
Jacob Stetser <jake@wideopenspac.es>
2014-04-27 14:12:19 -0600

    Got dotfiles working

--- modules/people/manifests/wideopenspaces/dotfiles.pp
+++ modules/people/manifests/wideopenspaces/dotfiles.pp
@@ -1,20 +1,31 @@
 class people::wideopenspaces::dotfiles {
- #  # My dotfile repository
- #  repository { "${my_sourcedir}/dotfiles":
- #    source => 'git@github.com:wideopenspaces/dotfiles.git',
- #  }
+  $my_username  = $::luser
+  $my_homedir   = "/Users/${my_username}"
+  $my_sourcedir = $::boxen_srcdir
 
- #  file { "/Users/${my_username}/.zshrc":
- #    ensure  => link,
- #    mode    => '0644',
- #    target  => "${my_sourcedir}/dotfiles/.zshrc",
- #    require => Repository["${my_sourcedir}/dotfiles"],
- #  }
+  # My dotfile repository
+  repository { "${my_sourcedir}/dotfiles":
+    source => 'wideopenspaces/dotfiles.git',
+  }
 
- # file { "/Users/${my_username}/.gitconfig":
- #    ensure  => link,
- #    mode    => '0644',
- #    target  => "${my_sourcedir}/dotfiles/.gitconfig",
- #    require => Repository["${my_sourcedir}/dotfiles"],
- #  }
+  file { "/Users/${my_username}/.zshrc":
+    ensure  => link,
+    mode    => '0644',
+    target  => "${my_sourcedir}/dotfiles/.zshrc",
+    require => Repository["${my_sourcedir}/dotfiles"],
+  }
+
+ file { "/Users/${my_username}/.gitconfig":
+    ensure  => link,
+    mode    => '0644',
+    target  => "${my_sourcedir}/dotfiles/.gitconfig",
+    require => Repository["${my_sourcedir}/dotfiles"],
+  }
+
+ file { "/Users/${my_username}/.profile":
+    ensure  => link,
+    mode    => '0644',
+    target  => "${my_sourcedir}/dotfiles/.profile",
+    require => Repository["${my_sourcedir}/dotfiles"],
+  }
 }
